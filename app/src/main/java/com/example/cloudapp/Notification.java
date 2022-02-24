a9c2a974bfa9ceff7ac9bf0641e0f0200bf01bed
katherinem7 <katherine.a.miller@uconn.edu>
2022-02-24 11:18:51 -0500

    Started basis for notification using find button -Katy and Payal

--- app/src/main/java/com/example/cloudapp/Notification.java
+++ app/src/main/java/com/example/cloudapp/Notification.java
@@ -1,14 +1,54 @@
 package com.example.cloudapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Notification extends AppCompatActivity {
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("1", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        android.app.Notification newMessageNotification = new NotificationCompat.Builder(this, "1")
+                .setContentTitle("Notification")
+                .setContentText("This is a notification")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        Button buttonShowNotification = findViewById(R.id.button2);
+        buttonShowNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notificationManager.notify(1, newMessageNotification);
+            }
+        });
     }
+
+
 }
