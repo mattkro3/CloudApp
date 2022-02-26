54125197b36b16355c5257a0d4941347863d4372
Payal Patel <payalben.patel@uconn.edu>
2022-02-25 22:26:50 -0500

    added the notification to the MainActivity

--- app/src/main/java/com/example/cloudapp/MainActivity.java
+++ app/src/main/java/com/example/cloudapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.cloudapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -35,11 +43,19 @@ public class MainActivity extends AppCompatActivity {
     private ApiClientFactory factory;
     private CreateTableAddRecordsAndReadAPIClient client;
     private TextView name2;
-
+    Button buttonNotify;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonNotify = findViewById(R.id.Notify);
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("My notification", "My notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
 
         name2 = (TextView) findViewById(R.id.Name2);
 
@@ -91,7 +107,23 @@ public class MainActivity extends AppCompatActivity {
                 name2.setText("");
             }
         });
+
+        buttonNotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "My notification");
+                builder.setContentTitle("Notification");
+                builder.setContentText("This is a notification");
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                managerCompat.notify(1, builder.build());
+            }
+        });
     }
+
+
 
     public boolean isInternetAvailable() {
         try {
--- app/src/main/java/com/example/cloudapp/Notification.java
+++ app/src/main/java/com/example/cloudapp/Notification.java
@@ -1,54 +1,52 @@
 package com.example.cloudapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class Notification extends AppCompatActivity {
+    Button buttonNotify;
 
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("1", name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
 
-        android.app.Notification newMessageNotification = new NotificationCompat.Builder(this, "1")
-                .setContentTitle("Notification")
-                .setContentText("This is a notification")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .build();
+        buttonNotify = findViewById(R.id.Notify);
 
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-        Button buttonShowNotification = findViewById(R.id.button2);
-        buttonShowNotification.setOnClickListener(new View.OnClickListener() {
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("My notification", "My notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        buttonNotify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                notificationManager.notify(1, newMessageNotification);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(Notification.this, "My notification");
+                builder.setContentTitle("Notification");
+                builder.setContentText("This is a notification");
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(Notification.this);
+                managerCompat.notify(1, builder.build());
             }
         });
+
     }
+
 
 
 }
